270d836dd16258d108b0ff69cf49bad6af6680b0
DaBlower <113282214+obobed@users.noreply.github.com>
2026-04-16 11:02:01 +1000

    add ability to either parse whole midi file or a range of bars

--- encode.py
+++ encode.py
@@ -4,35 +4,63 @@ import struct
 # --- CONFIG ---
 mid = mido.MidiFile('x.mid') # change the name here
 START_BAR, END_BAR = 1, 150 # 80, 96
-TICKS_PER_BAR = mid.ticks_per_beat * 4
-start_tick = (START_BAR - 1) * TICKS_PER_BAR
-end_tick = END_BAR * TICKS_PER_BAR
 
-all_notes_in_range = []
-active_notes = {}
 
-for track in mid.tracks:
-    current_tick = 0
-    for msg in track:
-        current_tick += msg.time
-        if msg.type == 'note_on' and msg.velocity > 0:
-            active_notes[msg.note] = current_tick
-        elif (msg.type == 'note_off' or (msg.type == 'note_on' and msg.velocity == 0)) and msg.note in active_notes:
-            st = active_notes.pop(msg.note)
-            if st >= start_tick and current_tick <= end_tick:
-                all_notes_in_range.append({
+def parse_midi_by_bars(mid_file, start_bar, end_bar):
+    ticks_per_bar = mid_file.ticks_per_beat * 4
+    start_tick = (start_bar - 1) * ticks_per_bar
+    end_tick = end_bar * ticks_per_bar
+
+    notes = []
+
+    for track in mid_file.tracks:
+        current_tick = 0
+        active_notes = {}
+        for msg in track:
+            current_tick += msg.time
+            if msg.type == 'note_on' and msg.velocity > 0:
+                active_notes[msg.note] = current_tick
+            elif (msg.type == 'note_off' or (msg.type == 'note_on' and msg.velocity == 0)) and msg.note in active_notes:
+                st = active_notes.pop(msg.note)
+                if st >= start_tick and current_tick <= end_tick:
+                    notes.append({
+                        'pitch': msg.note,
+                        'start': float(st - start_tick),
+                        'dur': float(current_tick - st)
+                    })
+
+    notes.sort(key=lambda x: x['start'])
+    return notes
+
+
+def parse_whole_midi(mid_file):
+    notes = []
+
+    for track in mid_file.tracks:
+        current_tick = 0
+        active_notes = {}
+        for msg in track:
+            current_tick += msg.time
+            if msg.type == 'note_on' and msg.velocity > 0:
+                active_notes[msg.note] = current_tick
+            elif (msg.type == 'note_off' or (msg.type == 'note_on' and msg.velocity == 0)) and msg.note in active_notes:
+                st = active_notes.pop(msg.note)
+                notes.append({
                     'pitch': msg.note,
-                    'start': float(st - start_tick),
+                    'start': float(st),
                     'dur': float(current_tick - st)
                 })
 
-all_notes_in_range.sort(key=lambda x: x['start'])
+    notes.sort(key=lambda x: x['start'])
+    return notes
 
 def float_to_bin(f):
     return bin(struct.unpack('!I', struct.pack('!f', f))[0])[2:].zfill(32)
 
 binary_output = ""
-for n in all_notes_in_range:
+#parsed_notes = parse_midi_by_bars(mid, START_BAR, END_BAR)
+parsed_notes = parse_whole_midi(mid)
+for n in parsed_notes:
     binary_output += format(n['pitch'], '08b')
     binary_output += float_to_bin(n['start'])
     binary_output += float_to_bin(n['dur'])
@@ -40,4 +68,4 @@ for n in all_notes_in_range:
 with open("result/full.txt", "w") as f:
     f.write(binary_output)
 
-print(f"Sorted binary saved, encoded {len(all_notes_in_range)} notes.")
+print(f"Sorted binary saved, encoded {len(parsed_notes)} notes.")
